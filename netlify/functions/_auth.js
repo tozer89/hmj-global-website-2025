e06127f968e9fb5b12755c71e6ec9d765e65aa56
tozer89 <40957153+tozer89@users.noreply.github.com>
2025-10-17 20:25:07 +0100

    Update _auth.js

--- netlify/functions/_auth.js
+++ netlify/functions/_auth.js
@@ -1,9 +1,16 @@
-const jwt = require('jsonwebtoken');
+// netlify/functions/_auth.js
+// Simple, dependency-free helper.
+// Netlify verifies the JWT and injects the user on context.clientContext.user.
 
-function getUser(context) {
-  const u = context.clientContext && context.clientContext.user;
-  if (!u) throw new Error('Unauthorized');
-  return u; // contains email, sub, app_metadata, etc.
+function unauthorized(message = 'Unauthorized') {
+  const err = new Error(message);
+  err.statusCode = 401;
+  return err;
 }
 
-module.exports = { getUser };
+exports.getUser = (context) => {
+  const user = context?.clientContext?.user;
+  if (!user) throw unauthorized();
+  // user.email, user.app_metadata, etc. are available here.
+  return user;
+};
